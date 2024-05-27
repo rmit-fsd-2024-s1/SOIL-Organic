7903328088d35e86cafae21b3f22134cd35b0197
jiasweet <s3958268@student.rmit.edu.au>
2024-05-27 21:25:14 +1000

    Update ShoppingCart.jsx
    Update Shopping Cart

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -5,27 +5,30 @@ import { useOutletContext } from "react-router-dom";
 function ShoppingCart() {
   const { cartItems, removeFromCart } = useOutletContext();
 
+  console.log(cartItems)
   return (
     <div className="flex flex-col justify-center items-center px-8">
       <h2 className="text-2xl">Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ul className="space-y-4">
-          {cartItems.map((cartItem, index) => (
-            <li key={index} className="flex items-center space-x-4">
-              <span>{cartItem.item}</span>
-              <span>Quantity: {cartItem.quantity}</span>
-              <button
-                className="bg-red-500 text-white px-2 py-1 rounded"
-                onClick={() => removeFromCart(cartItem)}
-              >
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+      <div>
+        {!cartItems || cartItems.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <ul className="space-y-4">
+            {cartItems && cartItems.map((cartItem, index) => (
+              <li key={index} className="flex items-center space-x-4">
+                <span>{cartItem.item}</span>
+                <span>Quantity: {cartItem.quantity}</span>
+                <button
+                  className="bg-red-500 text-white px-2 py-1 rounded"
+                  onClick={() => removeFromCart(cartItem)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
